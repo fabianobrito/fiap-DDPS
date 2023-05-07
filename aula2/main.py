bb1001fa297cef0bee01a0e683ce090d6c966fd1
Fabiano Antônio de Brito Farah <fabiano.farah@gmail.com>
2023-05-07 12:18:58 -0300

    Modificando script
    Melhorando o script e adicionando mais linhas

--- aula2/main.py
+++ aula2/main.py
@@ -1,68 +1,76 @@
 import os
-import pyodbc
+from pyhive import hive
 
 def iniciar_carga_hdfs():
     print("Inicio")
-    # print("=== Cria diretorios para transferir arquivos ====")
     
-    # os.system("docker exec namenode mkdir trabtwoddps")
-    # os.system("docker exec namenode hadoop fs -mkdir /app")
-    # os.system("docker exec namenode hadoop fs -mkdir /app/trabalho")
-    # os.system("docker exec namenode hadoop fs -mkdir /app/trabalho/hive")
-    # os.system("docker exec namenode hadoop fs -mkdir /app/trabalho/hive/address")
-    # os.system("docker exec namenode hadoop fs -mkdir /app/trabalho/hive/customer")
-    # os.system("docker exec namenode hadoop fs -mkdir /app/trabalho/hive/customeraddress")
-    # os.system("docker exec namenode hadoop fs -mkdir /app/trabalho/hive/product")
-    # os.system("docker exec namenode hadoop fs -mkdir /app/trabalho/hive/productcategory")
-    # os.system("docker exec namenode hadoop fs -mkdir /app/trabalho/hive/productdescription")
-    # os.system("docker exec namenode hadoop fs -mkdir /app/trabalho/hive/productmodel")
-    # os.system("docker exec namenode hadoop fs -mkdir /app/trabalho/hive/productmodeldescription")
-    # os.system("docker exec namenode hadoop fs -mkdir /app/trabalho/hive/salesorderdetail")
-    # os.system("docker exec namenode hadoop fs -mkdir /app/trabalho/hive/salesorderderhead")
-
-    # print("=== Povoar diretorios HDFS ====")
-    # os.system("docker exec namenode curl -k --url https://drive.google.com/file/d/1xiR-JmKVcL2r4EJkrJ-mqd8HInLdqasa/view?usp=share_link -o trabtwoddps/address.csv")
-    # os.system("docker exec namenode hadoop fs -put /trabtwoddps/address.csv /app/trabalho/hive/address")
-
-    # os.system("docker exec namenode curl -k --url https://drive.google.com/file/d/1j0i4sGBuY996uC8qcS8ZZvLTBN2YqK6a/view?usp=share_link -o trabtwoddps/customer.csv")
-    # os.system("docker exec namenode hadoop fs -put /trabtwoddps/address.csv /app/trabalho/hive/customer")
- 
-    # os.system("docker exec namenode curl -k --url https://drive.google.com/file/d/18YskJR7QaZPPbt6uBNN48eGQNYbUYGpB/view?usp=share_link -o trabtwoddps/customeraddress.csv")
-    # os.system("docker exec namenode hadoop fs -put /trabtwoddps/address.csv /app/trabalho/hive/customeraddress")
-
-    # os.system("docker exec namenode curl -k --url https://drive.google.com/file/d/18YskJR7QaZPPbt6uBNN48eGQNYbUYGpB/view?usp=share_link -o trabtwoddps/product.csv")
-    # os.system("docker exec namenode hadoop fs -put /trabtwoddps/address.csv /app/trabalho/hive/product")
-
-    # os.system("docker exec namenode curl -k --url https://drive.google.com/file/d/1AKgHJ1InA7YAAFwQNGhwY29kiL34L7bc/view?usp=share_link -o trabtwoddps/productcategory.csv")
-    # os.system("docker exec namenode hadoop fs -put /trabtwoddps/address.csv /app/trabalho/hive/productcategory")
-
-    # os.system("docker exec namenode curl -k --url https://drive.google.com/file/d/1Ybxo4GB7v73d1qacgVtCwnfSaQkQCOxS/view?usp=share_link -o trabtwoddps/productdescription.csv")
-    # os.system("docker exec namenode hadoop fs -put /trabtwoddps/address.csv /app/trabalho/hive/productdescription")
-
-    # os.system("docker exec namenode curl -k --url https://drive.google.com/file/d/1EwXN_iNohdnpgw-CogzeNNXpFp_ZwwYu/view?usp=share_link -o trabtwoddps/productmodel.csv")
-    # os.system("docker exec namenode hadoop fs -put /trabtwoddps/address.csv /app/trabalho/hive/productmodel")
-
-    # os.system("docker exec namenode curl -k --url https://drive.google.com/file/d/1x6GuS7pq2M_pqv1s0CIP6qiGXZaEjeZd/view?usp=share_link -o trabtwoddps/productmodeldescription.csv")
-    # os.system("docker exec namenode hadoop fs -put /trabtwoddps/address.csv /app/trabalho/hive/productmodeldescription")
-
-    # os.system("docker exec namenode curl -k --url https://drive.google.com/file/d/1ChW5hfwmD4l6llZIeLesvVZLUr9BnGu9/view?usp=share_link -o trabtwoddps/salesorderdetail.csv")
-    # os.system("docker exec namenode hadoop fs -put /trabtwoddps/address.csv /app/trabalho/hive/salesorderdetail")
-
-    # os.system("docker exec namenode curl -k --url https://drive.google.com/file/d/1K0uYlD9En3hP2u7e1cCLgF_gBydilDWI/view?usp=share_link -o trabtwoddps/salesorderderhead.csv")
-    # os.system("docker exec namenode hadoop fs -put /trabtwoddps/address.csv /app/trabalho/hive/salesorderderhead")
-    # os.system("docker exec namenode rm -rf trabtwoddps/")
-
-    os.system("docker exec hive-server hive -> show databases")
+    print("=== Cria diretorios para transferir arquivos ====")
+    os.system("sudo docker exec namenode mkdir trabtwoddps")
+    os.system("sudo docker exec namenode hadoop fs -mkdir /app")
+    os.system("sudo docker exec namenode hadoop fs -mkdir /app/trabalho")
+    os.system("sudo docker exec namenode hadoop fs -mkdir /app/trabalho/hive")
+    os.system("sudo docker exec namenode hadoop fs -mkdir /app/trabalho/hive/address")
+    os.system("sudo docker exec namenode hadoop fs -mkdir /app/trabalho/hive/customer")
+    os.system("sudo docker exec namenode hadoop fs -mkdir /app/trabalho/hive/customeraddress")
+    os.system("sudo docker exec namenode hadoop fs -mkdir /app/trabalho/hive/product")
+    os.system("sudo docker exec namenode hadoop fs -mkdir /app/trabalho/hive/productcategory")
+    os.system("sudo docker exec namenode hadoop fs -mkdir /app/trabalho/hive/productdescription")
+    os.system("sudo docker exec namenode hadoop fs -mkdir /app/trabalho/hive/productmodel")
+    os.system("sudo docker exec namenode hadoop fs -mkdir /app/trabalho/hive/productmodeldescription")
+    os.system("sudo docker exec namenode hadoop fs -mkdir /app/trabalho/hive/salesorderdetail")
+    os.system("sudo docker exec namenode hadoop fs -mkdir /app/trabalho/hive/salesorderderhead")
     
-    #create database if not exists AdventureWorks location '/app/trabalho/hive';
+    print("=== Povoar diretorios HDFS ====")
+    os.system("sudo docker exec namenode curl -Lb ./cookie 'https://drive.google.com/uc?export=download&id=1xiR-JmKVcL2r4EJkrJ-mqd8HInLdqasa' -o trabtwoddps/address.csv")
+    os.system("sudo docker exec namenode hadoop fs -rm /app/trabalho/hive/address/address.csv")
+    os.system("sudo docker exec namenode hadoop fs -put /trabtwoddps/address.csv /app/trabalho/hive/address")
+    
+    os.system("sudo docker exec namenode curl -Lb ./cookie 'https://drive.google.com/uc?export=download&id=1j0i4sGBuY996uC8qcS8ZZvLTBN2YqK6a' -o trabtwoddps/customer.csv")
+    os.system("sudo docker exec namenode hadoop fs -rm /app/trabalho/hive/customer/customer.csv")
+    os.system("sudo docker exec namenode hadoop fs -put /trabtwoddps/customer.csv /app/trabalho/hive/customer")
+    
+    os.system("sudo docker exec namenode curl -Lb ./cookie 'https://drive.google.com/uc?export=download&id=18YskJR7QaZPPbt6uBNN48eGQNYbUYGpB' -o trabtwoddps/customeraddress.csv")
+    os.system("sudo docker exec namenode hadoop fs -rm /app/trabalho/hive/customeraddress/customeraddress.csv")
+    os.system("sudo docker exec namenode hadoop fs -put /trabtwoddps/customeraddress.csv /app/trabalho/hive/customeraddress")
 
-    # print("=== Cria DataBase AdventureWorks ====")
-    # conn = pyodbc.connect("DSN=Hive_Connection", autocommit=True)
-    #conn = hive.Connection(host="localhost", port=10000, username="admin", password="admin", database="default", auth='CUSTOM')
-    #cursor = conn.cursor()
-    #cursor.execute("show databases")
-    #print(cursor.fetchall()) """
+    os.system("sudo docker exec namenode curl -Lb ./cookie 'https://drive.google.com/uc?export=download&id=18YskJR7QaZPPbt6uBNN48eGQNYbUYGpB' -o trabtwoddps/product.csv")
+    os.system("sudo docker exec namenode hadoop fs -rm /app/trabalho/hive/product/product.csv")
+    os.system("sudo docker exec namenode hadoop fs -put /trabtwoddps/product.csv /app/trabalho/hive/product")
+    
+    os.system("sudo docker exec namenode curl -Lb ./cookie 'https://drive.google.com/uc?export=download&id=1AKgHJ1InA7YAAFwQNGhwY29kiL34L7bc' -o trabtwoddps/productcategory.csv")
+    os.system("sudo docker exec namenode hadoop fs -rm /app/trabalho/hive/productcategory/productcategory.csv")
+    os.system("sudo docker exec namenode hadoop fs -put /trabtwoddps/productcategory.csv /app/trabalho/hive/productcategory")
+
+    os.system("sudo docker exec namenode curl -Lb ./cookie 'https://drive.google.com/uc?export=download&id=1Ybxo4GB7v73d1qacgVtCwnfSaQkQCOxS' -o trabtwoddps/productdescription.csv")
+    os.system("sudo docker exec namenode hadoop fs -rm /app/trabalho/hive/productdescription/productdescription.csv")
+    os.system("sudo docker exec namenode hadoop fs -put /trabtwoddps/productdescription.csv /app/trabalho/hive/productdescription")
+    
+    os.system("sudo docker exec namenode curl -Lb ./cookie 'https://drive.google.com/uc?export=download&id=1EwXN_iNohdnpgw-CogzeNNXpFp_ZwwYu' -o trabtwoddps/productmodel.csv")
+    os.system("sudo docker exec namenode hadoop fs -rm /app/trabalho/hive/productmodel/productmodel.csv")
+    os.system("sudo docker exec namenode hadoop fs -put /trabtwoddps/productmodel.csv /app/trabalho/hive/productmodel")
+
+    os.system("sudo docker exec namenode curl -Lb ./cookie 'https://drive.google.com/uc?export=download&id=1x6GuS7pq2M_pqv1s0CIP6qiGXZaEjeZd' -o trabtwoddps/productmodeldescription.csv")
+    os.system("sudo docker exec namenode hadoop fs -rm /app/trabalho/hive/productmodeldescription/productmodeldescription.csv")
+    os.system("sudo docker exec namenode hadoop fs -put /trabtwoddps/productmodeldescription.csv /app/trabalho/hive/productmodeldescription")
+    
+    os.system("sudo docker exec namenode curl -Lb ./cookie 'https://drive.google.com/uc?export=download&id=1ChW5hfwmD4l6llZIeLesvVZLUr9BnGu9' -o trabtwoddps/salesorderdetail.csv")
+    os.system("sudo docker exec namenode hadoop fs -rm /app/trabalho/hive/salesorderdetail/salesorderdetail.csv")
+    os.system("sudo docker exec namenode hadoop fs -put /trabtwoddps/salesorderdetail.csv /app/trabalho/hive/salesorderdetail")
+    
+    os.system("sudo docker exec namenode curl -Lb ./cookie 'https://drive.google.com/uc?export=download&id=1K0uYlD9En3hP2u7e1cCLgF_gBydilDWI' -o trabtwoddps/salesorderderhead.csv")
+    os.system("sudo docker exec namenode hadoop fs -rm /app/trabalho/hive/salesorderderhead/salesorderderhead.csv")
+    os.system("sudo docker exec namenode hadoop fs -put /trabtwoddps/salesorderderhead.csv /app/trabalho/hive/salesorderderhead")
+    
+    os.system("sudo docker exec namenode rm -rf trabtwoddps/")
+
+    print("=== Cria DataBase AdventureWorks ====")
+    conn = hive.Connection(host="localhost", port=10000, username="hive", password="hive", database="default", auth='CUSTOM')
+    cursor = conn.cursor()
+    cursor.execute("create database if not exists AdventureWorks location '/app/trabalho/hive'")
+    cursor.execute("show databases")
+    print(cursor.fetchall())
     print("Fim")
+
 
 if __name__ == '__main__':
     iniciar_carga_hdfs()
